2a6267987588fb083e34fd941cde746609520e54
MuhammadTalha123 <59221584+MuhammadTalha123@users.noreply.github.com>
2021-02-16 18:57:03 +0500

    app.js update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import React from 'react';
-import './App.css';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Home } from "./components/Home";
+import { AddUser } from "./components/AddUser";
+import { EditUser } from "./components/EditUser";
+import { GlobalProvider } from "./context/GlobalState";
 
-function App() {
+import "bootstrap/dist/css/bootstrap.min.css";
+
+const App = () => {
   return (
-    <div className="App">
-      <h1>App!</h1>
+    <div style={{ maxWidth: "30rem", margin: "4rem auto" }}>
+      <GlobalProvider>
+        <Router>
+          <Switch>
+            {/* <Route exact path="/" component={() => <Home users={users} setUsers={setUsers} />} /> */}
+            <Route exact path="/" component={Home} />
+            <Route path="/add" component={AddUser} />
+            <Route path="/edit/:id" component={EditUser} />
+          </Switch>
+        </Router>
+      </GlobalProvider>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
